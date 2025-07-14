f023fd3430572adbd2fb2c47a3268bd2ca1a8981
Andrés Moreno <122568735+andr3z1104@users.noreply.github.com>
2025-07-14 19:41:35 -0400

    Add dark footer with contact info

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,6 +5,7 @@ import { useAuth } from '@/contexts/AuthContext';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import MobileSidebar from './MobileSidebar';
+import Footer from './Footer';
 import { useIsMobile } from '@/hooks/use-mobile';
 
 const Layout: React.FC = () => {
@@ -16,14 +17,15 @@ const Layout: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-codomi-gray">
+    <div className="min-h-screen bg-codomi-gray flex flex-col">
       <Header />
-      <div className="flex">
+      <div className="flex flex-1">
         {isMobile ? <MobileSidebar /> : <Sidebar />}
         <main className="flex-1 p-4 md:p-6">
           <Outlet />
         </main>
       </div>
+      <Footer />
     </div>
   );
 };
